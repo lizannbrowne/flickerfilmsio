55c77c0149ab7e2575a65b38a0904df0baf81348
lizannbrowne <lizannbrowne@gmail.com>
2016-05-13 23:51:20 -0700

    deploy test

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -22,7 +22,7 @@ export default class CounterButton extends Component {
     className += ' btn btn-default';
     return (
       <button className={className} onClick={increment}>
-        You have clicked me TESTS {count} time{count === 1 ? '' : 's'}.
+        You have clicked me {count} time{count === 1 ? '' : 's'}!
       </button>
     );
   }
